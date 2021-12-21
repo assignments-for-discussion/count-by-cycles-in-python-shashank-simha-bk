a102bd8fc54eb81c3e8d59541fa5638735966207
Shashank Simha <67750571+shashank-simha-bk@users.noreply.github.com>
2021-12-21 13:17:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,27 @@
 
 def count_batteries_by_usage(cycles):
-    lowCount= 0,
-    mediumCount= 0,
-    highCount= 0
-    return {
-    if cycles<150:
-      lowCount=lowCount+1;
-      print('The charge cycle is low and the low count cycle is ' +lowCount)
-    elif cycles>150 or cycles<650:
-      mediumCount=mediumCount+1;
-      print('The charge cycle is medium and the medium count cycle is ' +mediumCount)
-    else cycles>650:
-      highCount=highCount+1;
-      print('The charge cycle is high and the high count cycle is ' +highCount)
-  
-  }
+    counts1={'lowCount':0,'mediumCount':0,'highCount':0}
+        for cycle in cycles:
+            if cycle<150:
+                counts['lowCount']+=1
+            elif cycle>=150 and cycle<650:
+                counts['mediumCount']+=1
+            else:
+                counts['highCount']+=1
+        return {
+            'lowCount':counts['lowCount'],
+            'mediumCount':counts['mediumCount'],
+            'highCount':counts['highCount']
+        }
 
 
 def test_bucketing_by_number_of_cycles():
   print("Counting batteries by usage cycles...\n");
   
   counts = count_batteries_by_usage([100, 300, 500, 600, 900, 1000])
-  # assert(counts["lowCount"] == 1)
- # assert(counts["mediumCount"] == 3)
-  #assert(counts["highCount"] == 2)
+  assert(counts["lowCount"] = 1)
+  assert(counts["mediumCount"] = 3)
+  assert(counts["highCount"] = 2)
   print("Done counting :)")
 
 
